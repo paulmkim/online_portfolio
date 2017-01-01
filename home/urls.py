767f92a2959bdf7bdfb2c45e4f5780d31379b4bd
Paul <paul.m.kime321@gmail.com>
2016-12-31 21:59:05 -0800

    Cooking Categories Added

--- home/urls.py
+++ home/urls.py
@@ -13,4 +13,16 @@ urlpatterns = [
 
 	#projects
 	url(r'projects/$', views.projects, name='projects'),
+
+	#food categories
+	url(r'cooking/highlights/$', views.highlights, name='highlights'),
+	url(r'cooking/breakfast/$', views.breakfast, name='breakfast'),
+	url(r'cooking/stock/$', views.stock, name='stock'),
+	url(r'cooking/fastCooking/$', views.fastCooking, name='fastCooking'),
+	url(r'cooking/salads/$', views.salads, name='salads'),
+	url(r'cooking/groundMeat/$', views.groundMeat, name='groundMeat'),
+	url(r'cooking/roasts/$', views.roasts, name='roasts'),
+	url(r'cooking/pasta/$', views.pasta, name='pasta'),
+	url(r'cooking/fried/$', views.fried, name='fried'),
+	url(r'cooking/desserts/$', views.desserts, name='desserts'),
 ]
--- home/views.py
+++ home/views.py
@@ -23,3 +23,34 @@ def poker(request):
 def projects(request):
 	context = {}
 	return render(request, 'home/projects.html', context)
+
+def highlights(request):
+	context = {}
+	return render(request, 'home/foodCategories/highlights.html', context)
+def breakfast(request):
+	context = {}
+	return render(request, 'home/foodCategories/breakfast.html', context)
+def stock(request):
+	context = {}
+	return render(request, 'home/foodCategories/stock.html', context)
+def fastCooking(request):
+	context = {}
+	return render(request, 'home/foodCategories/fastCooking.html', context)
+def salads(request):
+	context = {}
+	return render(request, 'home/foodCategories/salads.html', context)
+def groundMeat(request):
+	context = {}
+	return render(request, 'home/foodCategories/groundMeat.html', context)
+def roasts(request):
+	context = {}
+	return render(request, 'home/foodCategories/roasts.html', context)
+def pasta(request):
+	context = {}
+	return render(request, 'home/foodCategories/pasta.html', context)
+def fried(request):
+	context = {}
+	return render(request, 'home/foodCategories/fried.html', context)
+def desserts(request):
+	context = {}
+	return render(request, 'home/foodCategories/desserts.html', context)
